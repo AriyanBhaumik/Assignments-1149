32ad6f4e8b0974176a0deb4e085d13d37953fe47
AriyanBhaumik <97791233+AriyanBhaumik@users.noreply.github.com>
2023-05-05 11:13:52 +0530

    Add files via upload

---
+++ myclass.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class myclass
+{
+    public static void main(String args[]) {
+        int a;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter number= ");
+        a=in.nextInt();
+        System.out.println("Hello World !!\n"+a);
+        
+    }
+}
